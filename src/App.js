aa923da22a97fc4c6b1bd08220d54834fde74d07
Cormac <cormac.obroin6@gmail.com>
2022-03-08 08:51:55 +0000

    updated

--- src/App.js
+++ src/App.js
@@ -1,24 +1,94 @@
-import logo from './logo.svg';
-import './App.css';
+// App.js is the starting point and mose important component in our application
+// useEffect is a function that accepts another callback function, with a dependency array at the end
+import React, { useState, useEffect } from 'react'
+//  CssBaseline fixes whitespace and margins
+import { CssBaseline, Grid } from '@material-ui/core';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+import { getPlacesData, getWeatherData } from './api';
+import Header from './components/Header/Header';
+import List from './components/List/List';
+import Map from './components/Map/Map';
+
+const App = () => {
+const [places, setPlaces] = useState([]);
+
+const [weatherData, setWeatherData] = useState([]);
+const [filteredPlaces, setFilteredPlaces] = useState([]);
+const [childClicked, setChildClicked] = useState(null);
+
+const [coordinates, setCoordinates] = useState({});
+const [bounds, setBounds] = useState({});
+
+const [isLoading, setIsLoading] = useState(false);
+const [type, setType] = useState('restaurants');
+const [rating, setRating] = useState(3);
+
+// destructure navigator.geolocation.getCurrentPosition twice and make it our starting coordinates
+// this is the starting useState for our setCoordinates function
+// empty dependency array in this useEffect because it only needs to run at the start
+	useEffect(() => {
+	    navigator.geolocation.getCurrentPosition(({ coords: {latitude, longitude} }) => {
+	      setCoordinates({ lat: latitude, lng: longitude });
+	    })
+	  }, []);
+
+// this one chnages when rating is selected from the list
+	useEffect(() => {
+		const filtered = places.filter((place) => Number(place.rating) > rating);
+	
+		setFilteredPlaces(filtered);
+	  }, [rating]);
+
+// use .then in the useEffect because the api calls are asynchronous
+// .then has the data from the api calls which we can work with
+// to rerun the useEffect every time map/coordinates, type, bounds chnage we need to add dependencies "[type, bounds]"
+	useEffect(() => {
+		if(bounds.sw && bounds.ne){
+		  setIsLoading(true);
+
+		  getWeatherData(coordinates.lat, coordinates.lng)
+		  .then((data) => setWeatherData(data));
+
+		  getPlacesData(type, bounds.sw, bounds.ne)
+			.then((data) => {
+				console.log(data);
+
+			  	setPlaces(data?.filter((place) => place.name && place.num_reviews > 0));
+				setFilteredPlaces([]);
+				setIsLoading(false);
+			})
+		 }
+	  }, [type, bounds]);
+
+	return (
+		<>
+      <CssBaseline />
+      <Header setCoordinates={setCoordinates}/>
+      <Grid container spacing={3} style={{ width: '100%' }}>
+        <Grid item xs={12} md={4}>
+          	<List 
+		  		places={filteredPlaces.length ? filteredPlaces : places}
+				childClicked={childClicked}
+				isLoading={isLoading}
+				type={type}
+				setType={setType}
+				rating={rating}
+				setRating={setRating}
+		  	/>
+        </Grid>
+        <Grid item xs={12} md={8} style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+          <Map 
+		  	// setCoordinates is the function to get the coordinates state which we also use in map
+		  	setCoordinates={setCoordinates}
+		  	setBounds={setBounds}
+		  	coordinates={coordinates}
+			places={filteredPlaces.length ? filteredPlaces : places}
+			setChildClicked={setChildClicked}
+			weatherData={weatherData}
+		  />
+        </Grid>
+      </Grid>
+    </>
   );
 }
 
---
+++ src/components/Map/Map.jsx
@@ -0,0 +1,76 @@
+import React from 'react'
+import GoogleMapReact from 'google-map-react';
+import { Paper, Typography, useMediaQuery } from '@material-ui/core';
+import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined';
+import AccessibilityNewIcon from '@mui/icons-material/AccessibilityNew';
+import Rating from '@material-ui/lab/Rating';
+
+import useStyles from './styles';
+import mapStyles from './mapStyles';
+
+// isMobile is false if width of device is over 600px
+// to get bootstrapURLKey we set up a project in google developers console
+// GoogleMapReact tells us when coordinates change
+
+const Map = ({ places, setCoordinates, setBounds, setChildClicked, coordinates, weatherData }) => {
+	const classes = useStyles();
+	const matches = useMediaQuery('(min-width:600px)');
+
+	return (
+		<div className={classes.mapContainer}>
+      		<GoogleMapReact
+      		  	bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_MAPS_API_KEY }}
+      		  	defaultCenter={coordinates}
+      		  	center={coordinates}
+      		  	defaultZoom={14}
+      		  	margin={[50, 50, 50, 50]}
+				options={{ disableDefaultUI: true, zoomControl: true, styles: mapStyles }}
+      		  	onChange={(e) => {
+						console.log(e);
+						setCoordinates({ lat: e.center.lat, lng: e.center.lng });
+						setBounds({ ne: e.marginBounds.ne, sw: e.marginBounds.sw });
+					}}
+      		  	onChildClick={(child) => setChildClicked(child)}
+      		>
+			{coordinates ? 
+			<div
+			className={classes.userContainer}
+			lat={coordinates.lat}
+			lng={coordinates.lng}
+		  	>
+			<AccessibilityNewIcon color="secondary" fontSize="large" />
+  			</div>
+				: (console.log("None"))}
+			{places?.map((place, i) => (
+          		<div
+          		  className={classes.markerContainer}
+          		  lat={Number(place.latitude)}
+          		  lng={Number(place.longitude)}
+          		  key={i}
+          		>
+            {!matches
+              ? <LocationOnOutlinedIcon color="primary" fontSize="large" />
+              : (
+                <Paper elevation={3} className={classes.paper}>
+                  <Typography className={classes.typography} variant="subtitle2" gutterBottom> {place.name}</Typography>
+                  <img
+                    className={classes.pointer}
+                    src={place.photo ? place.photo.images.large.url : 'https://www.foodserviceandhospitality.com/wp-content/uploads/2016/09/Restaurant-Placeholder-001.jpg'}
+                  />
+                  <Rating name="read-only" size="small" value={Number(place.rating)} readOnly />
+                </Paper>
+              )}
+          </div>
+        ))}
+		{weatherData?.list?.length && weatherData.list.map((data, i) => (
+			// for each weather data we return a div
+          <div key={i} lat={data.coord.lat} lng={data.coord.lon}>
+            <img src={`http://openweathermap.org/img/w/${data.weather[0].icon}.png`} height="100px" />
+          </div>
+        ))}
+			</GoogleMapReact>
+		</div>
+	);
+}
+
+export default Map;
